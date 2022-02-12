7f7467c0060e948c7daf7ae7314a94251c8666b3
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-02-12 18:23:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let hash = location.href.split("#")[1];
+
+if (hash) {
+    let image = document.createElement('img');
+    document.body.style.margin = 0;
+    document.body.style.backgroundColor = "black";
+    image.src = `https://assets.scratch.mit.edu/get_image/.%2E/${hash}`;
+    image.style.width = "100%";
+    document.body.appendChild(image);
+}
