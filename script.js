ce451fbc35b79cad764a6fc1ae159e3311696dbf
MystPi <86574651+MystPi@users.noreply.github.com>
2022-03-21 10:37:41 -0400

    center image

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ if (hash) {
     let image = document.createElement('img');
     document.body.style.margin = 0;
     document.body.style.backgroundColor = "black";
+    document.body.style.display = "flex";
+    document.body.style.alignItems = "center";
+    document.body.style.justifyContent = "center";
+    document.body.style.height = "100vh";
     image.src = `https://assets.scratch.mit.edu/get_image/.%2E/${hash}`;
     image.style.maxWidth = "100%";
     document.body.appendChild(image);
