fc0295bcac927c2b29e14eb6ee2a98f174307c1f
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2022-03-31 15:10:50 -0700

    implimented requested

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const search = location.search.substring(1); // Gets the location.search, and then removes the first character
+const search = location.search.substring(1),
+    body = document.body; // Gets the location.search, and then removes the first character
 
-(function() {
-    if (!search) return; // if the search empty, then it stops.
-
+if (search) {
     // image
     let image = document.createElement("img");
-    image.src = `https://assets.scratch.mit.edu/get_image/../${search}`;
+    image.src = `https://assets.scratch.mit.edu/${search}`;
     image.style.backgroundColor = "white";
     image.style.maxWidth = "100%";
     image.style.margin = "auto";
@@ -13,23 +12,23 @@ const search = location.search.substring(1); // Gets the location.search, and th
     image.style.height = "auto";
 
     // title
-    document.title = search + " - View";
+    document.title = search + " - view";
 
     // body
-    document.body.innerHTML = ""
-    document.body.style.margin = 0;
-    document.body.style.backgroundColor = "black";
-    document.body.style.display = "flex";
-    document.body.style.alignItems = "center";
-    document.body.style.justifyContent = "center";
-    document.body.style.height = "100vh";
-    document.body.appendChild(image);
-})()
-
-function loadURL(element) {
-    let elem = element.value.split(".");
-    if (elem[0].substring(8) === "assets" && elem[1] === "scratch" && elem[2] === "mit" && elem[3].split("/")[0] === "edu")
-        location.assign(`?${element.value.substring(8).split("/")[3]}`);
-    else
-        alert(`${element.value} doesn't seem to be a url to Scratch Assets`);
+    body.innerHTML = ""
+    body.style.margin = 0;
+    body.style.backgroundColor = "black";
+    body.style.display = "flex";
+    body.style.alignItems = "center";
+    body.style.justifyContent = "center";
+    body.style.height = "100vh";
+    body.appendChild(image);
+} else {
+    document.getElementById("url").addEventListener("change", function() {
+        let elem = this.value.split(".");
+        if (elem[0].substring(8) === "assets" && elem[1] === "scratch" && elem[2] === "mit" && elem[3].split("/")[0] === "edu")
+            location.assign(`?${this.value.substring(8).split("/")[1]}`);
+        else
+            alert(`${this.value} doesn't seem to be a url to Scratch Assets`);
+    })
 }
