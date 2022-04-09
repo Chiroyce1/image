4dc93b3dfde854ba80fd13150e44e56f9f0dfd60
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-04-09 16:52:12 +0530

    ✨ Formatting and UX improvements

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let hash = location.href.split("#")[1];
+let loadBtn = document.querySelector('#load');
+let input = document.querySelector('#input')
 
-if (hash){
+if (hash) {
     // If the link is using the older version
     // Redirect them to the new one
     location.replace(`/image/?${hash}`)
@@ -32,15 +34,16 @@ if (search) {
     body.style.height = "100vh";
     body.appendChild(image);
 } else {
-    document.getElementById("url").addEventListener("change", function () {
-        let elem = this.value.split(".");
-        let elea = this.value.substring(8).split("/");
+    loadBtn.addEventListener("click", function () {
+        let elem = input.value.split(".");
+        let elea = input.value.substring(8).split("/");
         if (elem[0].substring(8) === "assets" && elem[1] === "scratch" && elem[2] === "mit" && elem[3].split("/")[0] === "edu") {
             let assign = 1;
             if (elea[1] == "get_image")
                 assign = 3;
             location.assign(`?${elea[assign]}`);
-        } else
+        } else {
             document.getElementById("error").innerText = "This URL doesn't seem to be a URL to Scratch Assets.";
+        }
     })
 }
