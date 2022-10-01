ff2217b4d9886b23cda3d84774bb484bacd7a6b4
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-10-01 21:57:52 +0530

    Refactor

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let hash = location.href.split("#")[1];
 let loadBtn = document.querySelector('#load');
-let input = document.querySelector('#url')
+let input = document.querySelector('#url');
 
 if (hash) {
-    // If the link is using the older version
-    // Redirect them to the new one
-    location.replace(`/image/?${hash}`)
+    // If the link is using the older version, redirect them to the new one
+    location.replace(`/image/?${hash}`);
 }
 
 const search = location.search.substring(1);
-let body = document.body; // Gets the location.search, and then removes the first character
+let body = document.body;
 
 if (search) {
     // image
@@ -34,13 +33,17 @@ if (search) {
     body.style.height = "100vh";
     body.appendChild(image);
 } else {
-    loadBtn.addEventListener("click", function () {
-        let elem = input.value.split(".");
-        let elea = input.value.substring(8).split("/");
-        if (elem[0].substring(8) === "assets" && elem[1] === "scratch" && elem[2] === "mit" && elem[3].split("/")[0] === "edu") {
-            let assign = 1;
-            if (elea[1] == "get_image")
-                assign = 3;
+    loadBtn.addEventListener("click", () => {
+        let url;
+
+        try {
+            url = new URL(input.value);
+        } catch (e) {
+            document.getElementById("error").innerText = "Invalid URL";
+            return;
+        }
+
+        if (url.origin === "https://assets.scratch.mit.edu") {
             location.assign(`?${elea[assign]}`);
         } else {
             document.getElementById("error").innerText = "This URL doesn't seem to be a URL to Scratch Assets.";
