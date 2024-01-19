ba50780df088f6c1cb2b5ed4613d841db4eaa3a6
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2024-01-19 10:31:10 +0530

    better design + responsive

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
 let hash = location.href.split("#")[1];
-let loadBtn = document.querySelector('#load');
-let input = document.querySelector('#url');
+let loadBtn = document.querySelector("#load");
+let input = document.querySelector("#url");
 
 if (hash) {
-    // If the link is using the older version, redirect them to the new one
-    location.replace(`/image/?${hash}`);
+	// If the link is using the older version, redirect them to the new one
+	location.replace(`/image/?${hash}`);
 }
 
-const encode = (arg) => arg.toLowerCase().replace(/\d/g, (n) => String.fromCharCode(71 + Number(n)));
+const encode = (arg) =>
+	arg.toLowerCase().replace(/\d/g, (n) => String.fromCharCode(71 + Number(n)));
 const decode = (arg) => arg.replace(/[G-P]/g, (s) => s.codePointAt(0) - 71);
 
 const search = location.search.substring(1);
-let body = document.body;
+
+function onInput() {
+	let url;
+
+	try {
+		url = new URL(input.value);
+	} catch (e) {
+		document.getElementById("error").innerText = "Invalid URL";
+		return;
+	}
+
+	if (url.origin === "https://assets.scratch.mit.edu") {
+		location.assign(`?${encode(url.pathname.slice(1))}`);
+	} else {
+		document.getElementById("error").innerText =
+			"This URL doesn't seem to be a URL to Scratch Assets.";
+	}
+}
 
 if (search) {
-    // image
-    let image = document.createElement("img");
-    const decoded = decode(search);
-    image.src = `https://assets.scratch.mit.edu/${decoded}`;
-    image.style.backgroundColor = "white";
-    image.style.maxWidth = "100%";
-    image.style.margin = "auto";
-    image.style.minWidth = "100px";
-    image.style.height = "auto";
+	// image
+	let image = document.createElement("img");
+	image.id = "img";
+	const decoded = decode(search);
+	image.src = `https://assets.scratch.mit.edu/${decoded}`;
 
-    // title
-    document.title = `${decoded} Assets Image Viewer`;
+	// title
+	document.title = `Scratch Assets Image Viewer`;
 
-    // body
-    body.innerHTML = "";
-    body.style.margin = 0;
-    body.style.backgroundColor = "black";
-    body.style.display = "flex";
-    body.style.alignItems = "center";
-    body.style.justifyContent = "center";
-    body.style.height = "100vh";
-    body.appendChild(image);
+	// body
+	document.body.innerHTML = "";
+	document.body.classList.add("view-img");
+	document.body.appendChild(image);
 } else {
-    loadBtn.addEventListener("click", () => {
-        let url;
-
-        try {
-            url = new URL(input.value);
-        } catch (e) {
-            document.getElementById("error").innerText = "Invalid URL";
-            return;
-        }
-
-        if (url.origin === "https://assets.scratch.mit.edu") {
-            location.assign(`?${encode(url.pathname.slice(1))}`);
-        } else {
-            document.getElementById("error").innerText = "This URL doesn't seem to be a URL to Scratch Assets.";
-        }
-    })
+	loadBtn.addEventListener("click", onInput);
+	document.querySelector("#url").addEventListener("keydown", (e) => {
+		if (e.key == "Enter") {
+			onInput();
+		}
+	});
 }
